0ceb58ce98161232b0a84590b0e2a7b99d82d525
Simon250388 <a.v.simonov@mail.ru>
2020-01-31 11:20:26 +0400

    fix build script

--- script/build.ps1
+++ script/build.ps1
@@ -1,2 +1,10 @@
+$appName = 'C:\Program Files\1cv8\8.3.15.1700\bin\1cv8.exe'
 $parentPath = ((Get-Item -Path '.\' -Verbose).Parent).FullName
-Get-ChildItem -Path $parentPath | ?{ $_.PSIsContainer } | Select-Object Name |Where-Object  {$_.Name -ne "script"} | foreach {$parentPath +"\"+ $_.Name}
+Get-ChildItem -Path $parentPath | ?{ $_.PSIsContainer } | Select-Object Name |Where-Object  {$_.Name -ne "script"} | foreach {
+    $bslName = $_.Name
+    $epfPath = ("$parentPath\$bslName\bin\$bslName.epf") 
+    write-host  $epfPath 
+    if ((Test-Path  $epfPath  ) -eq "True"){   
+    $folderBslName = ("$parentPath\$bslName")
+    & $appName "DESIGNER /LoadExternalDataProcessorOrReportFromFiles $folderBslName\$bslName.xml $folderBslName\bin\$bslName.epf"| Write-Host -NoNewline
+    }}
