e2d1a49b9c8f73fd3b8bc275c3a1369bbbd0898e
Simon250388 <a.v.simonov@mail.ru>
2020-01-31 11:35:28 +0400

    fix build script

--- script/build.ps1
+++ script/build.ps1
@@ -1,5 +1,5 @@
 $appName = 'C:\Program Files\1cv8\8.3.15.1700\bin\1cv8.exe'
-$parentPath = ((Get-Item -Path '.\' -Verbose).Parent).FullName
+$parentPath = (Split-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) -Parent)
 Get-ChildItem -Path $parentPath | ?{ $_.PSIsContainer } | Select-Object Name |Where-Object  {$_.Name -ne "script"} | foreach {
     $bslName = $_.Name
     $epfPath = ("$parentPath\$bslName\bin\$bslName.epf") 
