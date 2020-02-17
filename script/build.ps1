bd00fe3ea23449aed60ef922c012ec226c38bb11
Simon250388 <a.v.simonov@mail.ru>
2020-02-17 11:29:27 +0400

    fix build script

--- script/build.ps1
+++ script/build.ps1
@@ -1,14 +1,16 @@
 $curentPath = (Split-Path $MyInvocation.MyCommand.Path -Parent)
-[xml]$configFile= get-content $curentPath\Config.xml
-$PlatformPath = $configFile.configuration.Platform1c.add | Where { $_.Key -eq 'path' } | % { $_.value }
-$appName =  "$PlatformPath\bin\1cv8.exe"
+[xml]$configFile = get-content $curentPath\Config.xml
+$PlatformPath = $configFile.configuration.Platform1c.add | Where-Object { $_.Key -eq 'path' } | ForEach-Object { $_.value }
+$appName = "$PlatformPath\bin\1cv8.exe"
 $parentPath = (Split-Path (Split-Path $MyInvocation.MyCommand.Path -Parent) -Parent)
-Get-ChildItem -Path $parentPath | ?{ $_.PSIsContainer } | Select-Object Name |Where-Object  {$_.Name -ne "script"} | foreach {
-    $bslName = $_.Name
-    $epfPath = ("$parentPath\$bslName\bin\$bslName.epf") 
-    write-host  $epfPath 
-    if ((Test-Path  $epfPath  ) -eq "True"){
-	Remove-Item -Path $epfPath}  
-    $folderBslName = ("$parentPath\$bslName")
-    & $appName "DESIGNER /LoadExternalDataProcessorOrReportFromFiles $folderBslName\$bslName.xml $folderBslName\bin\$bslName.epf"| Write-Host -NoNewline
-    }
+Get-ChildItem -Path $parentPath | Where-Object { $_.PSIsContainer } | Select-Object Name | Where-Object { $_.Name -ne "script" } | ForEach-Object {
+   $bslName = $_.Name
+   $epfPath = ("$parentPath\$bslName\bin\$bslName.epf")
+   if ((Test-Path  $epfPath  ) -eq "True") {
+      Remove-Item -Path $epfPath
+   }
+   $folderBslName = ("$parentPath\$bslName")
+   & $appName "DESIGNER /LoadExternalDataProcessorOrReportFromFiles $folderBslName\$bslName.xml $folderBslName\bin\$bslName.epf" | Write-Host -NoNewline
+   write-host  $epfPath
+}
+Start-Sleep -s 10
